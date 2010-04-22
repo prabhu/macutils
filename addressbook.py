0687f5257f4715d54e384b407508b9ea2501a947
Prabhu <prabhu.subramanian@gmail.com>
2010-04-23 00:32:55 +0100

    Added couple of more methods.

--- addressbook.py
+++ addressbook.py
@@ -11,7 +11,10 @@ import sys
 import os
 
 # List of properties to be ignored.
-IGNORED_PROPS = ["com.apple.ABPersonMeProperty", "ABPersonFlags", "com.apple.ABImageData",]
+IGNORED_PROPS = ["com.apple.ABPersonMeProperty", "ABPersonFlags",
+                 "com.apple.ABImageData",
+                 "com.apple.ABGroupMembersProperty",
+                ]
 
 def getAddressBook():
     """
@@ -23,23 +26,40 @@ def all():
     """
     Returns all the people from the addressbook.
     """
-    peopleList = []
-    allPeople = getAddressBook().people()
-    for people in allPeople:
-        aperson = {}
-        for prop in people.allProperties():
+    return _clist(getAddressBook().people())
+
+def _clist(slist):
+    """
+    Method to convert NSArray to python list
+    """
+    retList = []
+    for p in slist:
+        aobj = {}
+        for prop in p.allProperties():
             if prop in IGNORED_PROPS:
                 continue
-            tmpval = people.valueForProperty_(prop)
+            tmpval = p.valueForProperty_(prop)
             if type(tmpval) == ABMultiValueCoreDataWrapper:
                 aval = [_getVal(tmpval.valueAtIndex_(i)) for i in range(0, tmpval.count())]
             else:
-                aval = _getVal(tmpval)                
+                aval = _getVal(tmpval)
             if aval is not None:
-                aperson[prop.lower()] = aval    
-        peopleList.append(aperson)
-    return peopleList
+                aobj[prop.lower()] = aval
+        retList.append(aobj)
+    return retList
 
+def groups():
+    """
+    Return groups
+    """
+    return _clist(getAddressBook().groups())
+
+def me():
+    """
+    Returns the current logged in user.
+    """
+    return _clist([getAddressBook().me()])[0]
+    
 def _getVal(tmpval):
     """
     Extract value from unicode or Date object.
@@ -52,10 +72,12 @@ def _getVal(tmpval):
     elif type(tmpval) == NSCFDictionary:
         aval = dict([(k.lower(), tmpval[k]) for k in tmpval.keys()])
     return aval
-    
-def main():
-    all()
 
+def main():
+    #print all()
+    #print groups()
+    print me()
+    
 if __name__ == '__main__':
 	main()
 
